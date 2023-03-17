f0da76aefec97720156bdb2be889eb7c472e7442
rodrigo.jara <rodrigo.jara@andbank.com>
2023-03-17 01:05:47 +0100

    added a groups template

--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -1,15 +1,19 @@
 import { IWord } from './../interfaces/IWord';
 import { IGroup } from './../interfaces/IGroup';
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Optional, Param } from '@nestjs/common';
 import { GroupsService } from './groups.service.js';
 
-@Controller('groups/:id')
+@Controller('groups/:id?')
 export class GroupsController {
 
     constructor(private readonly groupService: GroupsService) {}
+
     @Get()
-    getListTemplate(@Param('id') id:string): IGroup {
-       return this.groupService.getListTemplate(id);
+    getList(@Param('id') @Optional() id?:string): IGroup[] {
+        if (id)
+            return this.groupService.getList().filter(_=>_.Id == id);
+
+        return this.groupService.getList();
     }
 }
     
--- src/groups/groups.service.ts
+++ src/groups/groups.service.ts
@@ -1,29 +1,12 @@
 import { IGroup } from './../interfaces/IGroup';
 import { IWord } from './../interfaces/IWord';
 import { Injectable } from '@nestjs/common';
-import * as template1 from '../Resources/template1.json'
-import * as template2 from'../Resources/template2.json'
+import * as groups from '../Resources/groups.json'
 
 @Injectable()
 export class GroupsService {
-
-    getListTemplate(id: string): IGroup {
-        switch (id) {
-            case '1':
-                return { Id: template1.id, Name: template1.name, Words: this.parseCsv(decodeURIComponent(escape(atob(template1.text)))) };
-            case '2':
-                return { Id: template2.id, Name: template2.name, Words: this.parseCsv(decodeURIComponent(escape(atob(template2.text)))) };
-            case '3':
-                    return { Id: '3', Name: 'test', Words: [] };
-        }
+   
+    getList(): IGroup[] {
+       return groups.groups;
     }
-        
-    parseCsv(csv: string): IWord[] {
-        const rows = csv.trim().split('\n');
-        return rows.map((row) => {
-            const [name, value] = row.split('\t');
-            return { Name: name.trim(), Value: value.trim() };
-        });
 };
-
-}
